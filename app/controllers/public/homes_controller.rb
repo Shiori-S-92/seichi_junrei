aba239fc288b576ada5cdd002ad0fac90b3fb9f2
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-11-13 13:03:57 +0000

    [Update] admin/ログアウト

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,13 @@
 class Public::HomesController < ApplicationController
   def top
+    @posts = Post.all.page(params[:page]).per(10)
   end
 
   def about
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id, :title, :content, :postal_code, :address, :image)
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,16 +18,19 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-<script>
-  $(document).on('turbolinks:load', function() {
-    $('.jscroll-div').jscroll({ // 追加したdivのclass名と合わせる
-      contentSelector: '.jscroll',
-      nextSelector: '.next a',  // 次ページリンクのセレクタ
-      loadingHtml: '読み込み中',
-      padding: 10
-    });
+// ↓↓↓ 無限スクロール ↓↓↓
+$(document).on('turbolinks:load', function() {
+  $('.jscroll-div').jscroll({ // 追加したdivのclass名と合わせる
+    contentSelector: '.jscroll',
+    nextSelector: '.next a',  // 次ページリンクのセレクタ
+    loadingHtml: '読み込み中',
+    padding: 10
   });
-</script>
+});
+
+
+
+// ↓↓↓ 複数画像のスライド ↓↓↓
 
 // $(function() {
 //   $('.images').slick({
