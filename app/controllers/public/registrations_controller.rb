7a6fbf4920edc04338033b6c80b9efd96b2515a7
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-11-15 06:05:20 +0000

    [Update] user/新規登録,ログイン

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -9,22 +9,22 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # POST /resource
+  # # POST /resource
   # def create
   #   super
   # end
 
-  # GET /resource/edit
+  # # GET /resource/edit
   # def edit
   #   super
   # end
 
-  # PUT /resource
+  # # PUT /resource
   # def update
   #   super
   # end
 
-  # DELETE /resource
+  # # DELETE /resource
   # def destroy
   #   super
   # end
@@ -41,9 +41,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  ## name：一意性を持たせ、かつ2～20文字の範囲で設定
+  # validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+
 end
