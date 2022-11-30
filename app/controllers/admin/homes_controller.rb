bb64b0fb26d7c6265af9aedb728456899cddc176
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-11-30 12:16:49 +0000

    [Update] 投稿詳細

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HomesController < ApplicationController
   def top
     # @posts = Post.all
-    @posts = Post.all.page(params[:page]).per(10)
+    @posts = Post.all.page(params[:page])
   end
 
   private
