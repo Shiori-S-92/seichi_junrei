ecd427ad7463e139d919922e3cd89784ee88282a
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-12-04 05:44:45 +0000

    [Update] admin/post/show_投稿削除修正

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -16,7 +16,7 @@ class Admin::PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to admin_posts_path
+    redirect_to admin_top_path
   end
 
   private
